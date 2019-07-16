603932dd4c2a7cd1c1988a565a06ac51ce42b232
Emasiello <44810277+Emasiello@users.noreply.github.com>
2019-07-16 09:58:05 -0400

    Update code.js

--- code.js
+++ code.js
@@ -6,11 +6,11 @@ var url;
 onEvent("imageGoat", "click", function() {
   style = getText("text_inputCode");
   url = "https://www.goat.com/search?query="+style;
-  // open broswer on page url
+  window.location.replace(url);
 });
 
 onEvent("imageStockX", "click", function() {
   style = getText("text_inputCode");
   url = "https://stockx.com/search/sneakers?s="+style;
-  // open broswer on page url
+  window.location.replace(url);
 });
