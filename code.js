5d244feda3127a03ab060ee2f9e1c2dfc7ed53f8
Emasiello <44810277+Emasiello@users.noreply.github.com>
2019-07-16 10:11:05 -0400

    Update code.js

--- code.js
+++ code.js
@@ -9,8 +9,20 @@ onEvent("imageGoat", "click", function() {
   window.location.href = url;
 });
 
+onEvent("imageGoat", "tap", function() {
+  style = getText("text_inputCode");
+  url = "https://www.goat.com/search?query="+style;
+  window.location.href = url;
+});
+
 onEvent("imageStock", "click", function() {
   style = getText("text_inputCode");
   url = "https://stockx.com/search/sneakers?s="+style;
+  window.location.href = url;
+});
+
+onEvent("imageStock", "tap", function() {
+  style = getText("text_inputCode");
+  url = "https://www.stockx.com/search/sneakers?s="+style;
   window.location.href = url;
 });
