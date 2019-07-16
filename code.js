dd003574c19f20e33f3b7ed05aa038f78c5637dd
Emasiello <44810277+Emasiello@users.noreply.github.com>
2019-07-16 09:38:27 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+var style;
+var url;
+
+// When text is detected by camera change text_inputCode to match
+
+onEvent("imageGoat", "click", function() {
+  style = getText("text_inputCode");
+  url = "https://www.goat.com/search?query="+style;
+  // open broswer on page url
+});
+
+onEvent("imageStockX", "click", function() {
+  style = getText("text_inputCode");
+  url = "https://stockx.com/search/sneakers?s="+style;
+  // open broswer on page url
+});
